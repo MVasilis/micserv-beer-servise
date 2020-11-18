e5093a38e569344aa5a33ac4242612c488f7edfc
Vasilis <vasilis.martos@gmail.com>
2020-11-18 17:31:29 +0200

    first commit

---
+++ src/main/java/com/mservbeerservice/MservBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mservbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MservBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MservBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mservbeerservice/MservBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mservbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MservBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
